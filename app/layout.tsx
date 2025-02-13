652c9d5af12ae202f81a48a39cc54bc06de85d5e
Amanda So <soamanda95@gmail.com>
2025-02-12 19:02:11 -0800

    update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="flex-grow container mx-auto px-4 py-8">
+          {children}
+        </main>
       </body>
     </html>
   );
